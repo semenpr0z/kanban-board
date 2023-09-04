a1617a15eb463d64ccf202c7f5a59429e537cd56
Semen Prozov <justpr0z.com.ru@gmail.com>
2023-09-04 17:54:13 +0300

    added renaminColumn function

--- src/Views/Workspace/components/Column.vue
+++ src/Views/Workspace/components/Column.vue
@@ -9,10 +9,11 @@ export default {
     return {
       openModal: false,
       creationTask: false,
-      taskName: '',
+      taskName: "",
       invalidInput: false,
-      renamingColumn: false
-    }
+      renamingColumn: false,
+      cloneNameColumn: "",
+    };
   },
   components: {
     Task,
@@ -29,36 +30,53 @@ export default {
     },
     idColumn: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup() {
     const tasksStore = useTasksStore();
 
-
     return {
-      tasksStore
+      tasksStore,
     };
   },
   methods: {
     startStopCreationTask() {
-      this.creationTask = !this.creationTask
+      this.creationTask = !this.creationTask;
     },
     createTask(e) {
-      e.preventDefault()
+      e.preventDefault();
       if (!this.taskName.length) {
-        this.invalidInput = true
+        this.invalidInput = true;
       } else {
         this.tasksStore.createTask(this.column.id, this.taskName);
         this.startStopCreationTask();
       }
     },
-
+    startStopRenameColumn() {
+      this.cloneNameColumn = this.column.headerName;
+      this.renamingColumn = !this.renamingColumn;
+      console.log(this.cloneNameColumn)
+    },
+    renameColumn(e) {
+      e.preventDefault()
+      console.log('ok')
+      console.log(!this.cloneNameColumn.length)
+      if (!this.cloneNameColumn.length) {
+        this.invalidInput = true;
+      } else {
+        this.tasksStore.renameColumn(this.column.id, this.cloneNameColumn);
+        this.startStopRenameColumn();
+      }
+    },
   },
   watch: {
     taskName() {
-      this.invalidInput = false
-    }
+      this.invalidInput = false;
+    },
+    cloneNameColumn() {
+      this.invalidInput = false;
+    },
   },
 };
 </script>
@@ -66,28 +84,93 @@ export default {
 <template>
   <div class="column rounded shadow-sm">
     <header class="column__header">
-      <h6>{{ column.headerName }}</h6>
-      <div class="dropdown">
-        <threeDotBtn size="mini" class="" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" />
+      <h6 v-if="!renamingColumn">{{ column.headerName }}</h6>
+      <form class="renaming" @submit.prevent v-else>
+        <input
+          type="text"
+          class="form-control form-control-sm"
+          v-model="cloneNameColumn"
+        />
+        <button
+          type="button"
+          class="btn-close"
+          aria-label="Close"
+          @click="startStopRenameColumn"
+        ></button>
+        <button class="btn btn-sm btn-primary" type="submit" @click="renameColumn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-check-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"
+            />
+          </svg>
+        </button>
+      </form>
+      <div class="dropdown" v-if="!renamingColumn">
+        <threeDotBtn
+          size="mini"
+          class=""
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        />
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><button type="button" class="dropdown-item">Переименовать колонку</button></li>
-          <li><button type="button" class="dropdown-item">Удалить колонку</button></li>
+          <li>
+            <button
+              type="button"
+              class="dropdown-item"
+              @click="startStopRenameColumn"
+            >
+              Переименовать колонку
+            </button>
+          </li>
+          <li>
+            <button type="button" class="dropdown-item">Удалить колонку</button>
+          </li>
         </ul>
       </div>
     </header>
     <ul class="column__list">
       <Task v-for="task in column.tasks" :task="task" />
     </ul>
-    <button class="column__add-btn btn btn-sm" @click="startStopCreationTask" v-if="!creationTask">Добавить
-      задачу</button>
+    <button
+      class="column__add-btn btn btn-sm"
+      @click="startStopCreationTask"
+      v-if="!creationTask"
+    >
+      Добавить задачу
+    </button>
     <form class="column__creation-task" v-else>
-      <textarea @keyup.enter="createTask" class="form-control form-control-sm" :class="invalidInput ? 'is-invalid' : ''"
-        v-model="taskName" placeholder="Введите заголовок задачи"></textarea>
+      <textarea
+        @keyup.enter="createTask"
+        class="form-control form-control-sm"
+        :class="invalidInput ? 'is-invalid' : ''"
+        v-model="taskName"
+        placeholder="Введите заголовок задачи"
+      ></textarea>
       <div class="buttons">
-        <button class="btn btn-sm btn-primary" type="submit" @click="createTask">
+        <button
+          class="btn btn-sm btn-primary"
+          type="submit"
+          @click="createTask"
+        >
           Создать задачу
         </button>
-        <button type="button" class="btn-close " aria-label="Close" @click="startStopCreationTask"></button>
+        <button
+          type="button"
+          class="btn-close"
+          aria-label="Close"
+          @click="startStopCreationTask"
+        ></button>
       </div>
     </form>
   </div>
@@ -117,6 +200,24 @@ export default {
       margin: 0;
     }
 
+    .renaming {
+      display: flex;
+      gap: 5px;
+      width: 100%;
+      align-items: center;
+      padding: 0 5px;
+      margin-bottom: 5px;
+
+      input {
+        width: 70%;
+      }
+
+      .btn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
   }
 
   &__list {
